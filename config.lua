95aad0f8d0b6b82f36ab7038ec682312f8099e4e
MaDHouSe79 <madhouse1979@gmail.com>
2023-07-25 08:27:34 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config.AttachedKey = 38     -- 38 = E
 Config.AttacheKeyTxt = "E"  -- if AttachedKey = 38 this is E
 
 Config.Models = {
-    trucks = {'man', "trans_mbenzarocs",},
+    trucks = {'hauler',},
     trailers = {'tr2', 'trailersmall', 'boattrailer', 'trflat', 'cotrailer', 'flatbed3amber', 'trans-laso_trailer',},
     ramp = "imp_prop_flatbed_ramp" -- hash -901038522
 }
