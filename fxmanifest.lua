ba1dd05608c6de8f7ab0b7349efaa9db1874ba1e
MaDHouSe <madhouse1979@gmail.com>
2024-02-21 13:44:25 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,3 +1,6 @@
+--[[ ===================================================== ]] --
+--[[             MH Trailers Script by MaDHouSe            ]] --
+--[[ ===================================================== ]] --
 fx_version 'cerulean'
 game 'gta5'
 
@@ -5,22 +8,31 @@ author 'MaDHouSe'
 description 'MH-Trailers - to use trailers like in real life'
 version '1.0.0'
 
-
 shared_scripts {
-    '@qb-core/shared/locale.lua',
-    '@qb-core/shared/vehicles.lua',
-    'locales/en.lua', -- change to your language (en/nl) for now
+    --'@es_extended/imports.lua',   -- only if you use esx framework
+    --'@ox_lib/init.lua',           -- only if you use ex_lib
+    '@qb-core/shared/locale.lua',   -- only if you use qb framework
+    '@qb-core/shared/vehicles.lua', -- only if you use qb framework
+    'core/mconfig.lua',
+    'core/locale.lua',
+    'locales/*.lua',
     'config.lua',
 }
 
 client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
+    'core/cl_core.lua',
     'client/main.lua',
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
+    'core/sv_core.lua',
     'server/main.lua',
 }
-
 
 lua54 'yes'
