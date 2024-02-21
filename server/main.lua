05528fc1051899964667c627933d58e66ce2b678
MaDHouSe <madhouse1979@gmail.com>
2024-02-21 13:42:54 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,6 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
+--[[ ===================================================== ]] --
+--[[             MH Trailers Script by MaDHouSe            ]] --
+--[[ ===================================================== ]] --
 local trailers = {}
 
 local function isVehicleLoaded(trailerNetId, vehicleNetId)
@@ -19,7 +20,6 @@ local function AddVehicleToTrailer(trailerNetId, vehicleNetId)
     if trailers[trailerNetId] == nil then trailers[trailerNetId] = {} end
     if isVehicleLoaded(trailerNetId, vehicleNetId) then return end
     trailers[trailerNetId][#trailers[trailerNetId] + 1] = vehicleNetId
-    if Config.DebugTrailers then print(json.encode(trailers, {indent = true})) end
     TriggerClientEvent('mh-trailers:client:updateTrailers', -1, trailerNetId, trailers)
 end
 
@@ -30,19 +30,18 @@ local function RemoveVehicleFromTrailer(trailerNetId, vehicleNetId)
             trailers[trailerNetId][i] = nil
         end
     end
-    if Config.DebugTrailers then print(json.encode(trailers, {indent = true})) end
     TriggerClientEvent('mh-trailers:client:updateTrailers', -1, trailerNetId, trailers)
 end
 
-QBCore.Functions.CreateCallback("mh-trailers:server:pay", function(source, cb)
+CreateCallback("mh-trailers:server:pay", function(source, cb)
     local src = source
-    local player = QBCore.Functions.GetPlayer(src)
-    if player.Functions.GetMoney('cash') >= Config.Rent.shop.cost then
-        player.Functions.RemoveMoney("cash", Config.Rent.shop.cost, "rent-trailer-paid")
+    local player = GetPlayer(src)
+    if GetMoney(src, Config.MoneyType[Config.Framework]) >= Config.Rent.shop.cost then
+        RemoveMoney(src, Config.MoneyType[Config.Framework], Config.Rent.shop.cost, "rent-trailer-paid")
         cb(true)
     else
-        if player.Functions.GetMoney('bank') >= Config.Rent.shop.cost then
-            player.Functions.RemoveMoney("cash", Config.Rent.shop.cost, "rent-trailer-paid")
+        if GetMoney(src,'bank') >= Config.Rent.shop.cost then
+            RemoveMoney(src, Config.MoneyType[Config.Framework], Config.Rent.shop.cost, "rent-trailer-paid")
             cb(true)
         else
             cb(false)
@@ -50,8 +49,7 @@ QBCore.Functions.CreateCallback("mh-trailers:server:pay", function(source, cb)
     end
 end)
 
-
-QBCore.Functions.CreateCallback("mh-trailers:server:GetTrailerData", function(source, cb, trailerNetId)
+CreateCallback("mh-trailers:server:GetTrailerData", function(source, cb, trailerNetId)
     if trailers[trailerNetId] ~= nil then
         cb(trailers)
     else
@@ -74,24 +72,20 @@ end)
 
 RegisterNetEvent('mh-trailers:server:SpawnRamp')
 AddEventHandler('mh-trailers:server:SpawnRamp', function(source, _trailerNetID, _rampNetID)
-    local data = {trailer = _trailer, ramp = _ramp}
-    if Config.DebugRamp then print(json.encode(data, {indent = true})) end
+    local data = {trailer = _trailerNetID, ramp = _rampNetID}
     if isVehicleLoaded(_trailerNetID, _rampNetID) then return end
     TriggerClientEvent('mh-trailers:client:SpawnRamp', -1, data)
-    --AddRamoToTrailer(_trailerNetID, _rampNetID)
 end)
 
 RegisterNetEvent('mh-trailers:server:updateDoor')
 AddEventHandler('mh-trailers:server:updateDoor', function(_trailer, _door)
     local data = {trailer = _trailer, door = _door}
-    if Config.DebugDoor then print(json.encode(data, {indent = true})) end
     TriggerClientEvent('mh-trailers:client:updateDoor', -1, data)
 end)
 
 RegisterNetEvent('mh-trailers:server:updatePlatform')
 AddEventHandler('mh-trailers:server:updatePlatform', function(_trailer, _door)
     local data = {trailer = _trailer, door = _door}
-    if Config.DebugPlatform then print(json.encode(data, {indent = true})) end
     TriggerClientEvent('mh-trailers:client:updatePlatform', -1, data)
 end)
 
