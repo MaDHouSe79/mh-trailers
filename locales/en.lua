d98cc5a765771facf7297e0abd5904b91a7db554
MaDHouSe <madhouse1979@gmail.com>
2024-02-21 13:45:22 +0100

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,37 +1,31 @@
-local Translations = {
-    notify = {
-        ['already_on_trailer'] = "This vehicle is already on the trailer",
-        ['vehicle_must_be_stationary'] = "The vehicle must be stationary",
-        ['return_wrong_vehicle'] = "This is not the right vehicle to return!",
-        ['not_enough_money_to_rent'] = "You don't have enough money to rent a thing!",
-    },
-    target = {
-        ["get_in"] = "Get in",
-        ["ramp_up"] = "Ramp Up",
-        ["ramp_down"] = "Ramp Down",
-        ["platform_up"] = "Platform Up",
-        ["platform_down"] = "Platform Down",
-        ["place_ramp"] = "Place Ramp",
-        ["remove_ramp"] = "Remove Ramp",
-        ["lock_trailer"] = "Lock Trailer",
-        ["unlock_trailer"] = "Unlock Trailer",
-        ['rent_a_vehicle'] = "Rent a tuck and trailer",
-    },
-    info = {
-        ['press_boat_message'] = "Press [%{key}] to secure the boat",
-        ['press_other_message'] = "Press [%{key}] to secure the vehicle",
-        ['press_to_park'] = "Press [%{key}] tp park the vehicle.",
-    },
-    menu = {
-        ['select_header'] = "Select a truck and trailer",
-        ['select_truck'] = "Select truck",
-        ['select_trailer'] = "Select Trailer",
-        ['truck'] = "Truck",
-        ['trailer'] = "Trailer",
-    },
+--[[ ===================================================== ]] --
+--[[             MH Trailers Script by MaDHouSe            ]] --
+--[[ ===================================================== ]] --
+Locales['en'] = {
+    ['already_on_trailer'] = "This vehicle is already on the trailer",
+    ['vehicle_must_be_stationary'] = "The vehicle must be stationary",
+    ['return_wrong_vehicle'] = "This is not the right vehicle to return!",
+    ['not_enough_money_to_rent'] = "You don't have enough money to rent a thing!",
+    ['area_is_obstructed'] = "Area is obstructed",
+    --
+    ["get_in"] = "Get in",
+    ["ramp_up"] = "Ramp Up",
+    ["ramp_down"] = "Ramp Down",
+    ["platform_up"] = "Platform Up",
+    ["platform_down"] = "Platform Down",
+    ["place_ramp"] = "Place Ramp",
+    ["remove_ramp"] = "Remove Ramp",
+    ["lock_trailer"] = "Lock Trailer",
+    ["unlock_trailer"] = "Unlock Trailer",
+    ['rent_a_vehicle'] = "Rent a tuck and trailer",
+    --
+    ['press_boat_message'] = "Press [%s] to secure the boat",
+    ['press_other_message'] = "Press [%s] to secure the vehicle",
+    ['press_to_park'] = "Press [%s] to park the vehicle.",
+    --
+    ['select_header'] = "Select a truck and trailer",
+    ['select_truck'] = "Select truck",
+    ['select_trailer'] = "Select Trailer",
+    ['truck'] = "Truck",
+    ['trailer'] = "Trailer",
 }
-
-Lang = Lang or Locale:new({
-    phrases = Translations, 
-    warnOnMissing = true
-})
