b2a13cac1e061074f5338c7ebfe8b50c13a8c626
MaDHouSe <madhouse1979@gmail.com>
2024-02-21 13:44:56 +0100

    Add files via upload

---
+++ core/mconfig.lua
@@ -0,0 +1,37 @@
+--[[ ===================================================== ]] --
+--[[             MH Trailers Script by MaDHouSe            ]] --
+--[[ ===================================================== ]] --
+Locales = {}
+Config = {}
+
+-- Framework (Do not change this)
+Config.Framework = nil
+if GetResourceState("es_extended") ~= 'missing' then
+    Config.Framework = 'esx'
+elseif GetResourceState("qb-core") ~= 'missing' then
+    Config.Framework = 'qb'
+end
+
+if GetResourceState("LegacyFuel") ~= 'missing' then
+    Config.FuelScript= 'LegacyFuel'
+elseif GetResourceState("mh-fuel") ~= 'missing' then
+    Config.FuelScript = 'mh-fuel'
+end
+
+if GetResourceState("ox_lib") ~= 'missing' then
+    Config.Target = "ox_target" -- qb-target or ox_target
+    Config.Menu = "ox_lib"      -- qb-input or ox_lib
+elseif GetResourceState("ox_lib") == 'missing' then
+    Config.Target = "qb-target" -- qb-target or ox_target
+    Config.Menu = "qb-input"    -- qb-input or ox_lib
+end
+
+-- Notify System
+Config.NotifyTitle = "MH Trailers"
+
+-- Language
+Config.Locale = "en" -- use 'en' or 'nl' 
+
+-- Cash Settings
+Config.MoneySign = "€" -- (€/$)
+Config.MoneyType = {["qb"] = "cash", ['esx'] = "money"}
