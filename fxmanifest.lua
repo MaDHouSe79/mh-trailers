57c5e85e684a2eeba102ecc0b377301088184ac9
Opium Development <116782359+dotkee@users.noreply.github.com>
2024-02-26 15:04:54 -0500

    Fixed for the main QBCore issues.
    There is many more issues when using QBCore, but it is now usable.
    Any addon vehicles will not let you detach, and if more than 1 is on the trailer it will keep 1 stuck in the air.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,7 @@
 --[[ ===================================================== ]] --
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
 author 'MaDHouSe'
 description 'MH-Trailers - to use trailers like in real life'
@@ -10,7 +11,7 @@ version '1.0.0'
 
 shared_scripts {
     --'@es_extended/imports.lua', -- only if you use esx framework
-    --'@ox_lib/init.lua',         -- only if you use ex_lib
+    --'@ox_lib/init.lua', -- only if you use ox_lib
     'core/mconfig.lua',
     'core/locale.lua',
     'core/vehicles.lua',
@@ -35,5 +36,3 @@ server_scripts {
     'server/main.lua',
     'server/update.lua',
 }
-
-lua54 'yes'
