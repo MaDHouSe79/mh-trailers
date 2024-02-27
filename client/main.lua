6bb761daf69a8722b1752592ee4e6ded873aae0b
MaDHouSe <madhouse1979@gmail.com>
2024-02-27 10:12:31 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -18,22 +18,6 @@ local tmpTruck = nil
 local tmpTrailer = nil
 local rentPed = nil
 
-local entityEnumerator = {
-    __gc = function(enum)
-        if enum.destructor and enum.handle then
-            enum.destructor(enum.handle)
-        end
-        enum.destructor = nil
-        enum.handle = nil
-    end
-}
-
-local function DisplayHelpText(text)
-    SetTextComponentFormat('STRING')
-    AddTextComponentString(text)
-    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
-end
-
 local function Park()
     if IsPedInAnyVehicle(PlayerPedId()) then
         local veh = GetVehiclePedIsIn(PlayerPedId(), false)
@@ -257,29 +241,6 @@ local function RemoveVehicleFromTrailer(vehicle)
         end
         if Config.DebugTrailers then print(json.encode(trailers, {indent = true})) end
     end
-end
-
-local function EnumerateEntities(initFunc, moveFunc, disposeFunc)
-    return coroutine.wrap(function()
-        local iter, id = initFunc()
-        if not id or id == 0 then
-            disposeFunc(iter)
-            return
-        end
-        local enum = { handle = iter, destructor = disposeFunc }
-        setmetatable(enum, entityEnumerator)
-        local next = true
-        repeat
-            coroutine.yield(id)
-            next, id = moveFunc(iter)
-        until not next
-        enum.destructor, enum.handle = nil, nil
-        disposeFunc(iter)
-    end)
-end
-
-local function EnumerateVehicles()
-    return EnumerateEntities(FindFirstVehicle, FindNextVehicle, EndFindVehicle)
 end
 
 local function GetTrailerData(trailer)
